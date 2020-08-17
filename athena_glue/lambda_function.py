c3940fb89be229874c67608d7ca003edeb296189
Lucas Duarte <lucasdu@amazon.com>
2020-08-17 18:12:51 -0300

    fix(athena_function) adjusted event to get file from s3

--- athena_glue/lambda_function.py
+++ athena_glue/lambda_function.py
@@ -17,7 +17,7 @@ def read_tar_file(file_path):
 def aggregate_return_comprehend(file_name, comprehend_response):
     dict_count_type = {"ID" : file_name , "COMMERCIAL_ITEM" : 0, "DATE" : 0, "EVENT" : 0, 
         "LOCATION" : 0, "ORGANIZATION" : 0, "OTHER" : 0, "PERSON" : 0, "QUANTITY" : 0, "TITLE" : 0}
-    for entities in dict_comprehend_analysis["Entities"]:
+    for entities in comprehend_response["Entities"]:
         dict_count_type[entities.get("Type")] += 1
 
     return dict_count_type
@@ -44,10 +44,9 @@ def get_s3_file(bucket_name, file_path_s3, file_path_local):
 
 
 def lambda_handler(event, context):
-
     # Get the file from S3 this will be passed from event
     file_path_local = "/tmp/output.tar.gz"
-    file_path_s3 = "comprehend/output/936068047509-NER-12c6d76619e3bf93990c8866c7b08282/output/output.tar.gz"
+    file_path_s3 = event["Records"][0]["s3"]["object"]["key"]
     bucket_name = os.getenv("BUCKET_NAME", "")
 
     table_name = "book_insight_table"
@@ -60,7 +59,9 @@ def lambda_handler(event, context):
     get_s3_file(bucket_name, file_path_s3, file_path_local)
 
     lodaded_file = read_tar_file(file_path_local)
+    
     dict_comprehend_analysis = json.loads(lodaded_file)
+    
     aggregate_dictionary = aggregate_return_comprehend(file_name, dict_comprehend_analysis)
 
     df = pd.DataFrame([list(aggregate_dictionary.values())], columns=["ID", "COMMERCIAL_ITEM", "DATE", "EVENT", "LOCATION",
@@ -73,5 +74,3 @@ def lambda_handler(event, context):
 
     # Upload file to s3 and update the Glue Catalog
     covert_df_to_parquet(df, s3_path, database_name_glue, table_name_glue)
-
-
